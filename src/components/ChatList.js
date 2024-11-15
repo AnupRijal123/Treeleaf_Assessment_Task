8d7d963295b7353aec863657b91d6259d7fbbfaf
Anup <anuprijal321@gmail.com>
2024-11-16 01:25:25 +0545

    chat list css finished and added chat icon and colours

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,11 +1,17 @@
 import '../styles/ChatList.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function ChatList() {
     return (
         <>
             <div className="chat-list-heading">
                 <h4 className="chat-heading-text">Chats</h4>
-                <p className="chat-heading-icon">A</p>
+                <FontAwesomeIcon className="chat-heading-icon" icon="fa-solid fa-pen-to-square" />
+
             </div>
+
+
+
 
             <div className="chat-row">
                 <img className="chat-list-user-image"
@@ -13,9 +19,9 @@ function ChatList() {
                     alt="chat-user-photo"
                 />
                 <div className="username-and-message-container">
-                    <h5 className="username-text">ram_1991</h5>
+                    <h5 className="username-text">tom_1991</h5>
                     <div className="chat-preview-message">
-                        <p className="chat-preview-message-text">You sent a post asdasdasdasdasdasdasdadasd</p>
+                        <p className="chat-preview-message-text">You sent a post</p>
                         <div className="chat-preview-time-container">
                             <div className="dot-container">
                                 <p className="dot">.</p>
@@ -25,6 +31,46 @@ function ChatList() {
                     </div>
                 </div>
             </div>
+
+            <div className="chat-row active-chat-row">
+                <img className="chat-list-user-image"
+                    src={require('../assets/profileImage2.jpg')}
+                    alt="chat-user-photo"
+                />
+                <div className="username-and-message-container">
+                    <h5 className="username-text">cr7__</h5>
+                    <div className="chat-preview-message">
+                        <p className="chat-preview-message-text">k gardai ho?</p>
+                        <div className="chat-preview-time-container">
+                            <div className="dot-container">
+                                <p className="dot">.</p>
+                            </div>
+                            <p className="message-sent-time-text">2h</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="chat-row">
+                <img className="chat-list-user-image"
+                    src={require('../assets/profileImage3.jpg')}
+                    alt="chat-user-photo"
+                />
+                <div className="username-and-message-container">
+                    <h5 className="username-text">peterparker2002</h5>
+                    <div className="chat-preview-message">
+                        <p className="chat-preview-message-text">ma aja vyaudina voli vetaula hai</p>
+                        <div className="chat-preview-time-container">
+                            <div className="dot-container">
+                                <p className="dot">.</p>
+                            </div>
+                            <p className="message-sent-time-text">4d</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </>
     )
 }
